ae10f3a2ee30db6c9f3c542aab6635eee8bb3229
Jay Chen <jaychenjjc@gmail.com>
2026-05-01 19:19:14 -0400

    challenge 5 complete

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ function addGamesToPage(games) {
         let card = document.createElement('div');
         card.classList.add('game-card');
         card.innerHTML = `
-        <img src = "${games[i].img}"  class = "game-img"">
-        <p> ${games[i].name}</p>
-        <p>${games[i].description}</p>
+            <img src = "${games[i].img}"  class = "game-img"">
+            <p> ${games[i].name}</p>
+            <p>${games[i].description}</p>
         `;
         gamesContainer.appendChild(card);
     }
@@ -107,10 +107,11 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
-
+    let unfundedGames = GAMES_JSON.filter((game) =>{
+        return game.pledged <= game.goal;
+    });
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
@@ -118,18 +119,19 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let fundedGames = GAMES_JSON.filter((game) => {
+        return game.pledged >= game.goal; 
+    });
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fundedGames);
 }
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
-
+    addGamesToPage(GAMES_JSON);
     // add all games from the JSON data to the DOM
-
 }
 
 // select each button in the "Our Games" section
@@ -138,7 +140,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
--- pass.md
+++ pass.md
@@ -2,7 +2,7 @@ key 2:  OOZEdiveTRAPpine
 Key 3: 6games-container.stats-card15
 Key 4: 11seafoamGAMES_JSON
 Key 5: 19187800268BRAIN
-Key 6: 
+Key 6: 74FLANNELclick
 
 
 
